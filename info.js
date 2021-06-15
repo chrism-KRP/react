edf085c5a44e72e2ab929b91c662bf3e81247a15
chrism-KRP <85910377+chrism-KRP@users.noreply.github.com>
2021-06-14 17:11:29 -0700

    Add files via upload

---
+++ info.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { PropTypes } from "prop-types";
+
+class Info extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+    };
+  }
+    buttonPressed() {
+        this.setState({
+            count: this.state.count + 1,
+        });
+    }
+  
+  render() {
+    return (
+      <div>
+          <p>Count: {this.state.count} </p>
+          <button onClick={() => this.buttonPressed()}>Click Me!</button>
+      </div>
+    );
+  }
+}
+
+Info.defaultProps = {
+  number: 2,
+  text: "default",
+};
+
+export default Info;
